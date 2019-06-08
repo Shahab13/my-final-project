0206d1894a0f4a44167a763e67179210b43aee0c
Shahab13 <sam.autom@gmail.com>
2019-06-08 02:06:22 -0700

    list of cards rendered

--- my-final/src/Card.js
+++ my-final/src/Card.js
@@ -2,10 +2,11 @@ import React, { Component } from "react";
 
 export default class Card extends Component {
   state = {
-    bookData: ""
+    bookData: []
   };
   oldvalue = "";
   picLink = "";
+  myData = [];
   componentDidMount = () => {};
   titleChange = () => {
     console.log("zzzzzzzzzzzzzzzzzzz");
@@ -17,9 +18,12 @@ export default class Card extends Component {
       .then(data => {
         // this.setState({ bookData: data["items"] });
         // this.value = data["items"];
-        this.picLink = data["items"][0].volumeInfo.imageLinks.thumbnail;
-        console.log(this.picLink);
-        this.setState({ bookData: data["items"] });
+        // this.picLink = data["items"][0].volumeInfo.imageLinks.thumbnail;
+        //console.log(this.picLink);
+        this.setState({ bookData: data.items });
+        this.myData = data.items;
+        console.log(this.state.bookData);
+        //console.log(this.myData);
       })
       .catch(error => {
         this.setState({ error: true });
@@ -28,27 +32,38 @@ export default class Card extends Component {
   };
 
   render() {
-    // console.log(this.myApi);
-    if (this.oldvalue !== this.props.xx) {
-      this.titleChange();
-      this.oldvalue = this.props.xx;
-    }
-    return (
-      <div className="container">
-        <div className="card" style={{ width: "18rem" }}>
-          <img className="card-img-top" src={this.picLink} alt="Oops!" />
-          <div className="card-body my-card">
-            <h5 className="card-title">Title: {this.props.xx}</h5>
-            <p className="card-text">
-              sdfgsl;kdfjgsl;kdfjgslsldkjhflk skjdfhkgjhf
-              sdklfhkldhflkjdfhglksjhdfghdtyertyetr
-            </p>
-            <a href="#/" className="btn btn-primary">
-              More info
-            </a>
+    let temp = this.state.bookData.map((x, i) => {
+      return (
+        // <ol key={i}>{i}</ol>
+        <div className="container" key={i}>
+          <div className="card" style={{ width: "18rem" }}>
+            <img
+              className="card-img-top"
+              src={x.volumeInfo.imageLinks.thumbnail}
+              alt="Oops!"
+            />
+            <div className="card-body my-card">
+              <h5 className="card-title">Title: {x.volumeInfo.title}</h5>
+              <p className="card-text">{x.volumeInfo.description}</p>
+              <a href="#/" className="btn btn-primary">
+                More info
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    });
+    //console.log(this.myData);
+
+    if (this.props.xx !== "") {
+      if (this.oldvalue !== this.props.xx) {
+        this.titleChange();
+        this.oldvalue = this.props.xx;
+      }
+      console.log(this.myData);
+      return <div>{temp}</div>;
+    } else {
+      return <div className="container">Search somthing!</div>;
+    }
   }
 }
